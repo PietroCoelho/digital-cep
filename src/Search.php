25a60d7d258706c15d0c745125bcf5445517f749
Ton <stowpass@gmail.com>
2020-10-21 09:15:55 -0300

    Busca inicial no Viacep

---
+++ src/Search.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wead\DigitalCep;
+
+class Search{
+    private $url="https://viacep.com.br/ws/";
+
+    public function getAddressFromZipcode(string $zipCode){
+        $zipCode = preg_replace('/[^0-9]/mi','',$zipCode);
+        $get = file_get_contents($this->url.$zipCode."/json");
+        return (array)json_decode($get);
+    }
+   
+}
